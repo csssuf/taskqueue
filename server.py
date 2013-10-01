580558d20e33b78f4d083443a8f6b3e2eae2d47b
csssuf <csssuf724@gmail.com>
2013-10-01 09:16:24 -0400

    start addtask work

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 from tornado import websocket, web, ioloop
-import sqlite3 as sql
+#import sqlite3 as sql
 import configparser as cfgp
 
 cfgarr = None
+
 
 def main():
     global cfgarr
@@ -12,7 +13,9 @@ def main():
     app = web.Application([
         (r'/', IndexHandler),
         (r'/static/(.*)', web.StaticFileHandler, {'path':
-            cfgarr['core']['staticpath']})
+            cfgarr['core']['staticpath']}),
+        (r'/addtask', AddHandler),
+        (r'/tasks', TaskHandler)
         ])
     app.listen(cfgarr['core']['port'])
     ioloop.IOLoop.instance().start()
@@ -21,6 +24,19 @@ def main():
 class IndexHandler(web.RequestHandler):
     def get(self):
         self.render("templates/index.html", conf=cfgarr)
+
+
+class AddHandler(web.RequestHandler):
+    def get(self):
+        self.render("templates/addtask.html", conf=cfgarr)
+
+
+class TaskHandler(web.RequestHandler):
+    pass
+
+
+class WSHandler(websocket.WebSocketHandler):
+    pass
 
 if __name__ == '__main__':
     main()
